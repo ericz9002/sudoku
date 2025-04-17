f87382cc5aebb11cc93a10a65125c7fa12db3a8d
ericz9002 <69063833+ericz9002@users.noreply.github.com>
2025-04-17 11:00:06 -0400

    finished generateBoard script

--- src/scripts/generateBoard.js
+++ src/scripts/generateBoard.js
@@ -1,6 +1,4 @@
-import {checkOnlySolution, getNumSolutions} from "./checkOnlySolution.js"
-import {copyBoard} from "./copyBoard.js"
-import {printBoard} from "./printBoard.js"
+import {getNumSolutions} from "./checkOnlySolution.js"
 
 export function generateSolvedBoard(){
     let board = new Array(9).fill(0).map(()=>{return new Array(9).fill(null)});
@@ -8,21 +6,48 @@ export function generateSolvedBoard(){
     return board;  
 }
 
-//expects numStartingSquares to be >= 20
-export function generateBoard(numStartingSquares){
-    //todo
+//expects numStartingSquares to be >= 25
+export function generateBoard(numStartingSquares, timeoutTime = 500){
+    if(numStartingSquares < 25){
+        throw new Error('starting board should have at least 25 squares')
+    }
+    let board = generateBoardHelper(numStartingSquares, timeoutTime);
+    while(board === null){
+        board = generateBoardHelper(numStartingSquares, timeoutTime);
+    }
+    return board;
+}
+
+function generateBoardHelper(numStartingSquares, timeoutTime){
+    let startTime = new Date();
+    let board = generateSolvedBoard();
+    let filledPositions = new Set(Array.from(new Array(81).keys()));
+    let ctr = 0;
+    while(filledPositions.size > numStartingSquares){
+        let filledPositionsArr = [...filledPositions];
+        shuffleArray(filledPositionsArr);
+        let position = filledPositionsArr[0];
+        let row = Math.floor(position / 9);
+        let col = position % 9;
+        let prevVal = board[row][col];
+        board[row][col] = null;
+        if(getNumSolutions(board, [], true) !== 1){
+            board[row][col] = prevVal;
+        }
+        else{
+            filledPositions.delete(position);
+        }
+        let elapsedTime = new Date() - startTime;
+        if(elapsedTime > timeoutTime){
+            return null;
+        }
+    }
+    return board;
 }
 
 function backTrack(board, row, col){
-    let debug = false;
-    if(debug){
-        console.log(`row ${row}, col ${col}`)
-        printBoard(board)
-    }
     let availableMoves = [...getAvailableMoves(board,row,col)];
     shuffleArray(availableMoves);
-    if(debug)
-        console.log(`availableMoves is ${availableMoves}`)
     let backTrackVal = false;
     for(let availableMove of availableMoves){
         board[row][col] = availableMove;
@@ -33,8 +58,6 @@ function backTrack(board, row, col){
             backTrackVal = backTrack(board, row + 1, 0);
         }
         else if(row === 8 && col === 8){
-            if(debug)
-                console.log('returning true, last recursive call');
             return true;
         }
         if(backTrackVal === true){
@@ -44,8 +67,6 @@ function backTrack(board, row, col){
     if(backTrackVal === false){
         board[row][col] = null;
     }
-    if(debug)
-        console.log(`returning ${backTrackVal}`);
     return backTrackVal;
 
 }
@@ -85,12 +106,4 @@ function shuffleArray(array){
         const j = randInt(i, array.length);
         [array[j], array[i]] = [array[i], array[j]];
     }
-}
-
-for(let i = 0; i < 100; ++i){
-    let time1 = new Date();
-    let b = generateSolvedBoard();
-    let time2 = new Date();
-    //console.log(`${i}: solved board in ${(time2 - time1)/1000} seconds`);
-    //printBoard(b);
 }
